a7155050d2f8b8e0dee89ee48f986ccceba7e558
sarahssharkey <sharkeysarah1@gmail.com>
2018-02-04 16:26:05 +0000

    Add install script for ubuntu.

---
+++ install-sharkcoin.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3
+apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
+apt-get install libboost-all-dev
+cd sharkcoin
+contrib/install_db4.sh pwd
+export BDB_PREFIX='/db4'
+./autogen.sh 
+./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include"
+make
