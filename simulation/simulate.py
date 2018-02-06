e91e12fac5db591e6daf2455383bcc70d4262ab2
sarahssharkey <sharkeysarah1@gmail.com>
2018-02-06 18:51:46 +0000

    Update simulate.py to use bitcoinrpc instead.

--- simulation/simulate.py
+++ simulation/simulate.py
@@ -1,17 +1,22 @@
 from __future__ import print_function
-import requests, json, argparse
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--instance_num', help='Instance number')
 instance_num = parser.parse_args().instance_num
 
-rpcPort = 18444
-rpcUser = 'bitcoinrpc'
+rpc_port = 18444
+rpc_user = 'bitcoinrpc'
+rpc_password = 'sharkpass' + instance_num
 
-rpcPassword = 'sharkpass' + instance_num
-serverURL = 'http://' + rpcUser + ':' + rpcPassword + '@localhost:' + str(rpcPort)
+rpc_connection = AuthServiceProxy("http://{}:{}@127.0.0.1:{}".format(rpc_user, rpc_password, rpc_port))
+best_block_hash = rpc_connection.getbestblockhash()
+print(rpc_connection.getblock(best_block_hash))
 
-headers = {'content-type': 'application/json'}
-payload = json.dumps({"method": 'getpeerinfo', "jsonrpc": "2.0"})
-response = requests.get(serverURL, headers=headers, data=payload)
-print(response.json()['result'])
+# batch support : print timestamps of blocks 0 to 99 in 2 RPC round-trips:
+commands = [["getblockhash", height] for height in range(100)]
+block_hashes = rpc_connection.batch_(commands)
+blocks = rpc_connection.batch_([["getblock", h] for h in block_hashes])
+block_times = [block["time"] for block in blocks]
+print(block_times)
