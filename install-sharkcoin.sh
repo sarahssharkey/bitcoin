de46863e616365feeaef046030e7fef206073ac1
Sarah Sharkey <sasharke@tcd.ie>
2018-03-02 09:19:14 -0800

    Add libsodium install to install-sharkcoin and remove make.

--- install-sharkcoin.sh
+++ install-sharkcoin.sh
@@ -9,9 +9,8 @@ fi
 apt-get update
 apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3
 apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
-apt-get install libboost-all-dev
+apt-get install libboost-all-dev libsodium-dev
 contrib/install_db4.sh pwd
 export BDB_PREFIX='/db4'
 ./autogen.sh 
 ./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include"
-make
